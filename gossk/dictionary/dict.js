01d8a42812d864fe285c55df8f01a0c37035c913
artegoser <59178854+artegoser@users.noreply.github.com>
2021-10-23 20:23:27 +0300

    dict total

--- gossk/dictionary/dict.js
+++ gossk/dictionary/dict.js
@@ -1,5 +1,13 @@
 window.addEventListener("load", async ()=>{
     let dict = await fetch("/gossk/dictionary/slingonian-en-ru-dict.json");
     dict = await dict.json();
-    console.log(dict);
+    
+    $("#sub").on("click", ()=>{
+        $("#sub").val("");
+        for(let item of dict){
+            if(item.en.match(new RegExp($("#search").val(), "gi"))||item.ru.match(new RegExp($("#search").val(), "gi"))||item.sli.match(new RegExp($("#search").val(), "gi"))){
+                $("#sub").append(`SLI: ${item.sli} => RU: ${item.ru} => EN: ${item.en} <br/>`);
+            }
+        }
+    });
 });
